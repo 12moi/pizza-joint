aa155d8cb3d5221a2e02ba1dc5bf9183ad2883fa
12moi <moi.shadrack@student.moringaschool.com>
2022-01-14 15:02:54 +0300

    add validation to contact us form

--- assets/App/script.js
+++ assets/App/script.js
@@ -1,2 +1,33 @@
 
 
+// Validating the contact form
+function validation(){
+   let name=document.getElementById("name").value;
+   let email=document.getElementById("email").value;
+   let phone=document.getElementById("phone").value;
+   let error_message=document.getElementById("error_message");
+   var text;
+   error_message.style.padding="10px";
+   if(name.length <2){
+       text="Please enter valid Name";
+       error_message.innerHTML=text;
+       return false;
+   }
+   if(email.indexOf("@gmail.com") ==-1 || email.length <6){
+    text="Please enter valid Email";
+    error_message.innerHTML=text;
+    return false;
+  }
+  if(isNaN(phone)|| phone.length !=10){
+    text="Please enter valid Phone Number";
+    error_message.innerHTML=text;
+    return false;
+}else{
+    alert("Thank you for contacting us!");
+   return false;
+}
+// alert("Thank you for contacting us!");
+//    return false;
+//    myForm=document.getElementById("myForm").reset();
+//    alert("cleared")
+}
