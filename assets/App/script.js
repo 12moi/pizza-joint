e91427e2a36fe5168dc835c2336b1d270618918f
12moi <moi.shadrack@student.moringaschool.com>
2022-01-15 20:42:48 +0300

    validate delivery form

--- assets/App/script.js
+++ assets/App/script.js
@@ -5,69 +5,72 @@ $(function(){
     $('.customer_data').hide();
 
     $("#checkout").click(function(){
-        let size=$("#pizza-size option:selected").val();
-        let crust=$("#pizza-crust option:selected").val();
-        let topping=$("#pizza-topping option:selected").val();
-        let number=$("#pizza-number option:selected").val();
+        let Size=$("#pizza-size option:selected").val();
+        let Crust=$("#pizza-crust option:selected").val();
+        let Topping=$("#pizza-topping option:selected").val();
+        let Number=$("#pizza-number option:selected").val();
        
         // console.log(size); testing the function
 
-        let order=(s, c, t, n, total)=>{
-            return {s, c, t, n, total};
+        let order=(S, C, T, N, Total)=>{
+            return {S, C, T, N, Total};
         };
          
-        let price, totalPrice;
-        switch(size){
-            case size="normal":
+        let price, TotalPrice;
+        switch(Size){
+            case Size="normal":
                 price= 280;
-                if(crust=== "Thin crust"){
-                    totalPrice=(price*number)+150;
-                }else if(crust==="Neapolitan crust"){
-                    totalPrice=(price*number)+200;
-                }else if(crust==="Chicago Deep Dish"){
-                    totalPrice=(price*number)+250;
-                }else{
-                    totalPrice=(price*number)+300;
+                if(Crust=== "Thin crust"){
+                    TotalPrice=(price*number)+150;
+                }else if(Crust==="Neapolitan crust"){
+                    TotalPrice=(price*number)+200;
+                }else if(Crust==="Chicago Deep Dish"){
+                    TotalPrice=(price*number)+250;
                 }
+                // else{
+                //     TotalPrice=(price*number)+300;
+                // }
                 break;
-            case size="medium":
+            case Size="medium":
                 price=380;
-                if(crust=== "Thin crust"){
-                    totalPrice=(price*number)+150;
-                }else if(crust==="Neapolitan crust"){
-                    totalPrice=(price*number)+200;
-                }else if(crust==="Chicago Deep Dish"){
-                    totalPrice=(price*number)+250;
-                }else{
-                    totalPrice=(price*number)+300;
+                if(Crust=== "Thin crust"){
+                    TotalPrice=(price*number)+150;
+                }else if(Crust==="Neapolitan crust"){
+                    TotalPrice=(price*number)+200;
+                }else if(Crust==="Chicago Deep Dish"){
+                    TotalPrice=(price*number)+250;
                 }
+                // else{
+                //     TotalPrice=(price*number)+300;
+                // }
                 break;
-            case size="large":
+            case Size="large":
                 price=450;
-                if(crust=== "Thin crust"){
+                if(Crust=== "Thin crust"){
                     totalPrice=(price*number)+150;
-                }else if(crust==="Neapolitan crust"){
-                    totalPrice=(price*number)+200;
-                }else if(crust==="Chicago Deep Dish"){
-                    totalPrice=(price*number)+250;
-                }else{
-                    totalPrice=(price*number)+300;
+                }else if(Crust==="Neapolitan crust"){
+                    TotalPrice=(price*number)+200;
+                }else if(Crust==="Chicago Deep Dish"){
+                    TotalPrice=(price*number)+250;
                 }
+                // else{
+                //     TotalPrice=(price*number)+300;
+                // }
                 break;   
         }
-        switch(topping){
-            case topping="Fresh basil":
-                totalPrice=topping + 80;
+        switch(Topping){
+            case Topping="Fresh basil":
+                TotalPrice=Topping + 80;
                 break;
-        case topping="Sausage":
-                totalPrice=topping + 120;
+        case Topping="Sausage":
+                TotalPrice=Topping + 120;
                 break; 
-        case topping="black Olives":
-                totalPrice=topping + 180;
+        case Topping="black Olives":
+                TotalPrice=Topping + 180;
                 break;  
         }
         
-        let newOrder = order(size, crust, topping, number, totalPrice);
+        let newOrder = order(Size, Crust, Topping, Number, TotalPrice);
 
         console.log(newOrder);
         //Write to the order
@@ -105,6 +108,32 @@ $(function(){
     
 })
 
+
+// Validating the Delivery form
+function validate(){
+    let name=document.getElementById("username").value;
+    let location=document.getElementById("userlocation").value;
+    let phone=document.getElementById("userphone").value;
+    let DeliveranceNote=document.getElementById("DeliveranceNote");
+    var text;
+    //  DeliveranceNote.style.padding="10px";
+    if(name.length <2){
+        alert("Please enter valid Name");
+        
+    }
+    if(location.length <2){
+     alert("Please enter valid location");
+     
+   }
+   if(isNaN(phone)|| phone.length !=10){
+     alert("Please enter valid Phone Number")
+     
+ }else{
+     alert("Thank you your Pizza will be delivered to you!");
+    return false;
+ }
+  document.getElementById("deliverlocation").reset();
+ }
 
 // Validating the contact form
 function validation(){
